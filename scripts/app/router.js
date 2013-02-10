92ff4e9a00c25cccb59ae919008bbeb1dd61fe5f
Andreas Savvides <andreas@editd.com>
2013-02-10 23:17:56 +0000

    Updating the router to make use of the view manager.

--- scripts/app/router.js
+++ scripts/app/router.js
@@ -1,19 +1,23 @@
 // The router is responsible for routing client-side pages
 // and associating them with actions and events.
-define(function(require, exports) {
+define(function(require) {
 
 	"use strict";
 
-	var Backbone = require("backbone");
+	var Backbone = require("backbone"),
+		HomeView = require("views/home"),
+		viewManager = require("views/view_manager");
 
 	return Backbone.Router.extend({
+
+		viewManager: viewManager,
 
 		routes : {
 			"": "home",
 		},
 
 		home: function() {
-			console.log("...home!");
+			this.viewManager.showView(new HomeView());
 		}
 
 	});
